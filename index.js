e0cc80cc85c1afe423e6a6c2bec8c2527dfbe0b4
nahid <nahidkishore99@gmail.com>
2020-10-05 18:49:14 +0600

    database connected

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require('express')
-const app = express()
-const port=5000
+    const express = require('express')
+    const app = express()
+    const MongoClient = require('mongodb').MongoClient;
+    require('dotenv').config()
+    const port=7000
+  
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
 
-app.listen(port)
+
+
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jolmh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true  });
+    client.connect(err => {
+      const collection = client.db("test").collection("devices");
+    console.log('database connected');
+      client.close();
+    });
+
+
+    app.get('/', function (req, res) {
+      res.send('hello world')
+    })
+
+
+    app.listen(port)
