1e17f734e7f08db26a81942cd9da4f233dbe6453
nahid <nahidkishore99@gmail.com>
2020-10-05 22:49:26 +0600

    email filter done

--- index.js
+++ index.js
@@ -3,17 +3,44 @@
     const MongoClient = require('mongodb').MongoClient;
     require('dotenv').config()
     const port=7000
-  
-
-
-
+    const bodyParser= require('body-parser');
+    const cors= require('cors');
 
     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jolmh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+    app.use(cors());
+    app.use(bodyParser.json());
+
     const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true  });
     client.connect(err => {
-      const collection = client.db("test").collection("devices");
-    console.log('database connected');
-      client.close();
+      const registerEventsCollection = client.db("volunteerNetwork").collection("events");
+    
+app.post('/addRegisteredEvent',(req,res) => {
+  const newRegistration =req.body;
+  registerEventsCollection.insertOne(newRegistration)
+  .then(result => {
+   
+    res.send(result.insertedCount > 0)
+  })
+  console.log(newRegistration);
+})
+
+app.get('/events',(req, res)=>{
+  //console.log(req.body.email);
+  registerEventsCollection.find({email:req.query.email})
+  .toArray((err,documents)=>{
+    res.send(documents)
+  })
+})
+   //delete
+ app.delete('/delete/:id',(req, res)=>{
+  /*  console.log(req.params.id); */
+ registerEventsCollection.deleteOne({_id: ObjectId (req.params.id)})
+ .then((result)=>{
+    console.log(result);
+ res.send('everything is fine')
+ })
+ })
     });
 
 
